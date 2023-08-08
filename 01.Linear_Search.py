16b688246e79fe777053fe9f01c196ccd14f8188
Prince Pandey <vickypp007961@gmail.com>
2023-08-08 10:41:00 +0530

    Added Linear Search python file

---
+++ 01.Linear_Search.py
@@ -0,0 +1,19 @@
+def linear_search(lst, target):
+    for i in range(len(lst)):
+        if lst[i] == target:
+            return i
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index:",index)
+    else:
+        print("Target not found in list")
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = linear_search(numbers, 13)
+verify(result)
+
+result = linear_search(numbers, 3)
+verify(result)
