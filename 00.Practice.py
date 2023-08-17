f1c4f32eb4fe9ae1833ce0fe10f689abafcc0b04
Prince Pandey <vickypp007961@gmail.com>
2023-08-17 13:22:10 +0530

    added array

--- 00.Practice.py
+++ 00.Practice.py
@@ -1,6 +1,18 @@
-#Adding 2 list
+# Adding 2 list
 
-numbers = [1,2,3,4,5]
-numbers01 = [6,7,8,9]
-numbers.extend(numbers01)
-print(numbers)
+# numbers = [1,2,3,4,5]
+# numbers01 = [6,7,8,9]
+# numbers.extend(numbers01)
+# print(numbers)
+
+# Array
+
+import array as myarray
+arr1 = myarray.array("i",[1,2,3,4,5,67,9])        # creating array
+print(arr1)
+
+lst = list(range(1,101))
+arr2 = myarray.array("i",lst)
+
+print(lst)
+print(arr2)
