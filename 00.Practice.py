49ec15a27a7dc5c7ae15e5029695c8ae5ca021be
Prince Pandey <vickypp007961@gmail.com>
2023-08-22 19:04:22 +0530

    Updated practice file

--- 00.Practice.py
+++ 00.Practice.py
@@ -4,15 +4,3 @@
 # numbers01 = [6,7,8,9]
 # numbers.extend(numbers01)
 # print(numbers)
-
-# Array
-
-import array as myarray
-arr1 = myarray.array("i",[1,2,3,4,5,67,9])        # creating array
-print(arr1)
-
-lst = list(range(1,101))
-arr2 = myarray.array("i",lst)
-
-print(lst)
-print(arr2)
