b36350b88190348d025a57e73cecacf4f5353bfc
Prince Pandey <vickypp007961@gmail.com>
2023-08-27 03:03:32 +0530

    Average Temperature using list

---
+++ 05.AvgTemperature.py
@@ -0,0 +1,16 @@
+import numpy as np
+high_temp = []
+input1 = int(input("How many day's temperature? "))
+for i in range(input1):
+    input2 = int(input(f"Day {i+1}'s high temperature: "))
+    high_temp.append(input2)
+print()
+
+Average = round(sum(high_temp) / len(high_temp),2)
+print(f"Average = {Average}")
+
+temp = 0
+for temperature in high_temp:
+    if temperature > Average:
+        temp += 1
+print(f"{temp} day(s) above average")
