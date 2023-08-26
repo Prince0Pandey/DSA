724341c9fbafc223710332ffafdd7b0baa54b958
Prince Pandey <vickypp007961@gmail.com>
2023-08-26 12:38:05 +0530

    Added linear search

--- 03.List.py
+++ 03.List.py
@@ -1,55 +1,83 @@
-lst = ["milk", "cheese", "butter", "rice"]
-print("milk" in lst)        # in operator   prints True
-print("bread" in lst)       # in operator   prints False
+# lst = ["milk", "cheese", "butter", "rice"]
+# print("milk" in lst)        # in operator   prints True
+# print("bread" in lst)       # in operator   prints False
 
 # for i in range(len(lst)):
 #     lst[i] += "+"
 #     print(lst[i])
 
+
 """Insertion in list"""
-mylst = [1,2,3,4,5,6,7,8]
-mylst[3] = 12
-mylst[5] = 9
-print(mylst)
+
+# mylst = [1, 2, 3, 4, 5, 6, 7, 8]
+# mylst[3] = 12
+# mylst[5] = 9
+# print(mylst)
+
 # Time Complexity:  O(1)        # Time taken to insert an element is same therefore O(1)
 # Space Complexity: O(1)        # insertion does not require any space as memory is already reserved.
+
 
 """ Insert/Update using insert(), append(), extend() """
 
 # 1. Inserting an element to the beginning of the list
-mylst[0] = "Tree"
-print(mylst)
+
+# mylst[0] = "Tree"
+# print(mylst)
+
 # Time Complexity:  O(1)
 # Space Complexity: O(1)
 
 
 # 2. Inserting an element to any given place of the list
-mylst.insert(6,"House")
-print(mylst)
+
+# mylst.insert(6, "House")
+# print(mylst)
+
 # Time Complexity:  O(n)
 # Space Complexity: O(1)
 
 
 # 3. Inserting an element to the end of the list
-mylst.append("DSA")
-print(mylst)
+
+# mylst.append("DSA")
+# print(mylst)
+
 # Time Complexity:  O(1)
 # Space Complexity: O(1)
 
 
 # 4. Inserting another list to the list
-mynewlst = [9,10,11,12]
-mylst.extend(mynewlst)
-print(mylst)
+
+# mynewlst = [9,10,11,12]
+# mylst.extend(mynewlst)
+# print(mylst)
+
 # Time Complexity:  O(N) Here N is number of element in "mynewlst"
 # Space Complexity: O(N) Here N is number of element in "mynewlst"
 
 
+'''Searching for an element in the list'''
+
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+target = 500
+
+# 1. in operator
+if target in my_list:
+    print(f"{target} is in the list")
+else:
+    print(f"{target} is not in the list")
+# Time Complexity: O(N)
 
 
+# 2. Linear Search
+def linear_search(p_list:list, p_target:int):
+    for i, value in enumerate(p_list):          # O(N)
+        if value == p_target:                   # O(1)
+            return i                            # O(1)
+    return -1                                   # O(1)
+print(linear_search(my_list, target))
 
-
-
-
+# Time Complexity: O(n)
 
 
